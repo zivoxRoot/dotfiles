f46306d3ff9f5c8ba583e1bef48332155631c455
zivoxRoot <theophiledefeche@ik.me>
2026-05-13 15:07:35 +0200

    add bc calculator

--- home/packages.nix
+++ home/packages.nix
@@ -6,6 +6,7 @@
   home.packages = with pkgs; [
     inputs.ayugram-desktop.packages.${pkgs.system}.ayugram-desktop
     inputs.fsel.packages.${pkgs.system}.default
+    bc
     wl-clipboard
     adwaita-icon-theme
     satty
