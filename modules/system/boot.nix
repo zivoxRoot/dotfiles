fee42fc9db911fcead35ed3556ad88cab021827f
zivoxRoot <theophiledefeche@ik.me>
2026-05-16 11:43:08 +0200

    add i2c devices to boot

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -3,6 +3,11 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # For ddcutil to manage brightness on external monitor
+  boot.kernelModules = ["i2c-dev"];
+  hardware.i2c.enable = true;
+  users.users.theophile.extraGroups = ["i2c"];
+
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 }
