b643cebdd2f528fc91a8dc05010443408b980474
zivoxRoot <theophiledefeche@ik.me>
2026-05-31 19:16:25 +0200

    add startup apps

--- home/programs/hyprland/startup.nix
+++ home/programs/hyprland/startup.nix
@@ -1,9 +1,13 @@
-{...}: {
+{ ... }:
+{
   wayland.windowManager.hyprland.settings.exec-once = [
     "waybar"
     "clipse -listen"
     "swww-daemon"
     "swaync"
     "udiskie --tray --automount --notify"
+    "Telegram"
+    "sioyek"
+    "foot --app-id=yazi yazi"
   ];
 }
