ce2ccc589d8a453083978a83426c90796c9cc7c4
zivoxRoot <theophiledefeche@ik.me>
2026-06-03 14:06:08 +0200

    update package ordering/grouping

--- home/packages.nix
+++ home/packages.nix
@@ -5,53 +5,60 @@
 }:
 {
   home.packages = with pkgs; [
-    inputs.fsel.packages.${pkgs.system}.default
-    unixtools.arp
-    libqalculate
-    lazydocker
-    pulsemixer
-    qutebrowser
-    nyxt
+    # Misc
+    fastfetch
+
+    # Desktop
     gnome.gvfs
     udiskie
     exfatprogs
     ntfs3g
-    sesh
+    swww
+    brightnessctl
+    bluez
+    xwayland-satellite
+    libnotify
+    playerctl
+    wl-clipboard
+    adwaita-icon-theme
+    qutebrowser
+
+    # GUI applications
+    telegram-desktop
+    vscode
+    mpv
+    nautilus
+    satty
+    feh
+
+    # TUI tools
+    bluetui
+    impala
+    ani-cli
+    yazi
+    yt-dlp
+    fzf
+    pulsemixer
+    lazygit
     gomi
+    htop
+    inputs.fsel.packages.${pkgs.system}.default
+    libqalculate
+    lazydocker
+
+    # CLI tools
+    tree
+    vim
+    unzip
+    glow
+    sesh
     ripgrep
     fd
     bat
     jq
-    htop
-    telegram-desktop
+    unixtools.arp
+
+    # Programming
     python315
-    lazygit
-    glow
-    vscode
-    wl-clipboard
-    adwaita-icon-theme
-    satty
-    feh
-    fzf
-    pulsemixer
-    playerctl
-    ani-cli
-    tree
-    yazi
-    yt-dlp
-    libnotify
-    mpv
-    nautilus
-    alejandra
-    pre-commit
-    vim
-    xwayland-satellite
-    swww
-    brightnessctl
-    fastfetch
-    bluez
-    bluetui
-    impala
-    unzip
   ];
 }
